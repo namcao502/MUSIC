8fcbd4f52a4c29e8f7321167d2af43444667af5e
unknown <namcao502@gmail.com>
2022-11-08 19:30:46 +0700

    double back to leave, fix some fragment transaction problems

--- app/src/main/java/com/example/music/online/ui/activities/LOGActivity.kt
+++ app/src/main/java/com/example/music/online/ui/activities/LOGActivity.kt
@@ -2,18 +2,37 @@ package com.example.music.online.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.music.R
 import com.example.music.utils.checkNetwork
+import com.example.music.utils.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AccountActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+
+    private var doubleBackToExitPressedOnce = false
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        toast("Please click BACK again to exit")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            doubleBackToExitPressedOnce = false }
+            , 2000)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
